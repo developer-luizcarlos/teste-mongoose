34d51129ea68cbb0e5945c56ac00bbb8bf39c68f
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2024-11-14 14:15:02 -0300

    .

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,12 @@
 import express,{ Express } from "express";
 import router from "./routers/routes";
+import cors from 'cors';
 import "dotenv/config";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/",router);
 
 export default app;
